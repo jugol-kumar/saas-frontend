dc7c896fc8cbc9e22c779a9b548b0876893c5f38
TusharImran-Dev <tusharimran.dev@gmail.com>
2024-02-19 17:37:39 +0600

    Invoice Pages Style Updated

--- pages/invoice/edit.vue
+++ pages/invoice/edit.vue
@@ -240,12 +240,12 @@
               </div>
               <div class="card bg-glass-morphi glass-morphi-border">
                 <div class="card-body d-flex align-items-center gap-3 flex-column">
-                  <button class="primary-button w-100 mb-75" data-bs-toggle="modal" data-bs-target="#send-invoice-sidebar">
+                  <button class="primary-button w-100 mb-75" data-bs-toggle="offcanvas" data-bs-target="#send-invoice-sidebar">
                     Send Invoice
                   </button>
                   <a href="" class="glass-morphi-button text-center w-100 mb-75">Preview</a>
                   <button type="button" class="glass-morphi-button w-100 mb-75">Save</button>
-                  <button class="primary-button w-100 mb-75" data-bs-toggle="modal" data-bs-target="#add-payment-sidebar">
+                  <button class="primary-button w-100 mb-75" data-bs-toggle="offcanvas" data-bs-target="#add-payment-sidebar">
                     Add Payment
                   </button>
                 </div>
@@ -273,98 +273,67 @@
           </div>
 
           <!-- Send Invoice Sidebar -->
-          <Offcanvas ><Offcanvas/>
-          <div class="modal modal-slide-in fade" id="send-invoice-sidebar" aria-hidden="true">
-            <div class="modal-dialog sidebar-lg">
-              <div class="modal-content p-0">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
-                <div class="modal-header mb-1">
-                  <h5 class="modal-title">
-                    <span class="align-middle">Send Invoice</span>
-                  </h5>
-                </div>
-                <div class="modal-body flex-grow-1">
-                  <form>
-                    <div class="mb-1">
-                      <label for="invoice-from" class="form-label">From</label>
-                      <input type="text" class="form-control" id="invoice-from" value="shelbyComapny@email.com" placeholder="company@email.com" />
-                    </div>
-                    <div class="mb-1">
-                      <label for="invoice-to" class="form-label">To</label>
-                      <input type="text" class="form-control" id="invoice-to" value="qConsolidated@email.com" placeholder="company@email.com" />
-                    </div>
-                    <div class="mb-1">
-                      <label for="invoice-subject" class="form-label">Subject</label>
-                      <input type="text" class="form-control" id="invoice-subject" value="Invoice of purchased Admin Templates" placeholder="Invoice regarding goods" />
-                    </div>
-                    <div class="mb-1">
-                      <label for="invoice-message" class="form-label">Message</label>
-                      <textarea class="form-control" name="invoice-message" id="invoice-message" cols="3" rows="11">
-Dear Queen Consolidated,
-
-Thank you for your business, always a pleasure to work with you!
-
-We have generated a new invoice in the amount of $95.59
-
-We would appreciate payment of this invoice by 05/11/2019</textarea>
-                    </div>
-                    <div class="mb-1">
-                                            <span class="badge badge-light-primary">
-                                                <i data-feather="link" class="me-25"></i>
-                                                <span class="align-middle">Invoice Attached</span>
-                                            </span>
-                    </div>
-                    <div class="mb-1 d-flex flex-wrap mt-2">
-                      <button type="button" class="btn btn-primary me-1" data-bs-dismiss="modal">Send</button>
-                      <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
-                    </div>
-                  </form>
-                </div>
+          <Offcanvas id="send-invoice-sidebar">
+            <form>
+              <div class="mb-1">
+                <label for="invoice-from" class="form-label">From</label>
+                <input type="text" class="form-control" id="invoice-from" value="shelbyComapny@email.com" placeholder="company@email.com" />
               </div>
-            </div>
-          </div>
+              <div class="mb-1">
+                <label for="invoice-to" class="form-label">To</label>
+                <input type="text" class="form-control" id="invoice-to" value="qConsolidated@email.com" placeholder="company@email.com" />
+              </div>
+              <div class="mb-1">
+                <label for="invoice-subject" class="form-label">Subject</label>
+                <input type="text" class="form-control" id="invoice-subject" value="Invoice of purchased Admin Templates" placeholder="Invoice regarding goods" />
+              </div>
+              <div class="mb-1">
+                <label for="invoice-message" class="form-label">Message</label>
+                <textarea class="form-control bg-transparent text-white" name="invoice-message" id="invoice-message" cols="3" rows="11">Dear Queen Consolidated, Thank you for your business, always a pleasure to work with you! We have generated a new invoice in the amount of $95.59 We would appreciate payment of this invoice by 05/11/2019</textarea>
+              </div>
+              <div class="mb-1">
+              <span class="d-flex align-items-center my-3 gap-2">
+                  <Icon name="material-symbols-light:link" size="24" />
+                  <span class="align-middle">Invoice Attached</span>
+              </span>
+              </div>
+              <div class="mb-1 d-flex flex-wrap mt-2">
+                <button type="button" class="primary-button me-1" data-bs-dismiss="offcanvas">Send</button>
+                <button type="button" class="glass-morphi-button" data-bs-dismiss="offcanvas">Cancel</button>
+              </div>
+            </form>
+          </Offcanvas>
           <!-- /Send Invoice Sidebar -->
 
           <!-- Add Payment Sidebar -->
-          <div class="modal modal-slide-in fade" id="add-payment-sidebar" aria-hidden="true">
-            <div class="modal-dialog sidebar-lg">
-              <div class="modal-content p-0">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
-                <div class="modal-header mb-1">
-                  <h5 class="modal-title">
-                    <span class="align-middle">Add Payment</span>
-                  </h5>
-                </div>
-                <div class="modal-body flex-grow-1">
-                  <form>
-                    <div class="mb-1">
-                      <input id="balance" class="form-control" type="text" value="Invoice Balance: 5000.00" disabled />
-                    </div>
-                    <div class="mb-1">
-                      <label class="form-label" for="amount">Payment Amount</label>
-                      <input id="amount" class="form-control" type="number" placeholder="$1000" />
-                    </div>
-                    <div class="mb-1">
-                      <label class="form-label" for="payment-date">Payment Date</label>
-                      <input id="payment-date" class="form-control date-picker" type="text" />
-                    </div>
-                    <div class="mb-1">
-                      <label class="form-label" for="payment-method">Payment Method</label>
-                      <v-section />
-                    </div>
-                    <div class="mb-1">
-                      <label class="form-label" for="payment-note">Internal Payment Note</label>
-                      <textarea class="form-control" id="payment-note" rows="5" placeholder="Internal Payment Note"></textarea>
-                    </div>
-                    <div class="d-flex flex-wrap mb-0">
-                      <button type="button" class="btn btn-primary me-1" data-bs-dismiss="modal">Send</button>
-                      <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
-                    </div>
-                  </form>
-                </div>
+          <Offcanvas id="add-payment-sidebar">
+            <form>
+              <div class="mb-1">
+                <input id="balance" class="form-control" type="text" value="Invoice Balance: 5000.00" disabled />
               </div>
-            </div>
-          </div>
+              <div class="mb-1">
+                <label class="form-label" for="amount">Payment Amount</label>
+                <input id="amount" class="form-control" type="number" placeholder="$1000" />
+              </div>
+              <div class="mb-1">
+                <label class="form-label" for="payment-date">Payment Date</label>
+                <input id="payment-date" class="form-control date-picker" type="text" />
+              </div>
+              <div class="mb-1">
+                <label class="form-label" for="payment-method">Payment Method</label>
+                <v-select />
+              </div>
+              <div class="mb-1">
+                <label class="form-label" for="payment-note">Internal Payment Note</label>
+                <textarea class="form-control bg-transparent text-white" id="payment-note" rows="5"></textarea>
+              </div>
+              <div class="d-flex flex-wrap mb-0">
+                <button type="button" class="primary-button me-1" data-bs-dismiss="offcanvas">Send</button>
+                <button type="button" class="glass-morphi-button" data-bs-dismiss="offcanvas">Cancel</button>
+              </div>
+            </form>
+          </Offcanvas>
+
           <!-- /Add Payment Sidebar -->
         </section>
 
