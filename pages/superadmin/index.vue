9b496aa296aace65fbb1e5129964316c1bcd9da1
jugol-kumar <jk23717933@gmail.com>
2024-03-04 13:22:25 +0600

    add supper admin page

---
+++ pages/superadmin/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+ definePageMeta({
+    middleware:['auth'],
+    layout:"admin"
+  })
+</script>
+
+
+<template>
+    <h1 class="text-white">Super admin ................</h1>
+</template>
+
