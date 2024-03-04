e8a06733997b49c8a0aaff751ba4357646f98d8c
jugol-kumar <jk23717933@gmail.com>
2024-03-04 13:43:20 +0600

    work in plan module

--- pages/plans.vue
+++ pages/plans.vue
@@ -1,13 +1,25 @@
 <script setup>
+import {useToast} from "vue-toastification";
+import {useTokenStore} from "~/stores/useTokenStore.js";
+const toast = useToast();
+
+
+const {data, error, pending} = useApiFetch(`customer/plans`)
+watch(error, ()=>{
+  if(error?.value){
+    toast.error(error?.value?.data?.message)
+  }
+})
+
 </script>
 
 <template>
-  <div class="row flex-wrap mt-5">
-    <PlanCard />
-    <PlanCard />
-    <PlanCard />
-    <PlanCard />
-  </div>
-  <DataTable />
 
+  <div v-if="pending">
+    <h1>Loading......</h1>
+  </div>
+
+  <div v-else class="row flex-wrap mt-5">
+    <PlanCard :info="item" v-for="item in data" :key="`single-plan-card-${item.id}`"/>
+  </div>
 </template>
